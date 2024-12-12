1d4987b5c64272bc9859fbd10899d6ce7a5dd8e0
Cuong1711BL <huynhphucuong2001bl@gmail.com>
2024-12-13 00:35:28 +0700

    add code to end server

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,14 @@
 const request = require('supertest');
-const app = require('../index');
+const app = require('../index'); // Import your app
+let server;
+
+beforeAll(() => {
+  server = app.listen(3000); // Start the server before tests
+});
+
+afterAll(() => {
+  server.close(); // Close the server after tests
+});
 
 describe('GET /', () => {
   it('should respond with "Hello, World!"', async () => {
