a0f5aed6ba29511b7e1ecaab244f36f038702a27
Cuong1711BL <huynhphucuong2001bl@gmail.com>
2024-12-15 09:52:18 +0700

    update script

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.get('/', (req, res) => {
       <main>
         <h2>Home Page</h2>
         <p>This is the home page of the Express web application. Use the navigation menu to explore other pages.</p>
+        <p>This is for master branch<p>
       </main>
       <footer>
         <p>&copy; 2024 Express Demo</p>
