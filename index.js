a8f9ddb6bb7d575f1f2cd5efdc1ea299ec366784
Cuong1711BL <huynhphucuong2001bl@gmail.com>
2024-12-15 10:00:11 +0700

    update master 2

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.get('/', (req, res) => {
         <h2>Home Page</h2>
         <p>This is the home page of the Express web application. Use the navigation menu to explore other pages.</p>
         <p>This is for master branch 1<p>
+        <p>This is for master branch 2<p>
       </main>
       <footer>
         <p>&copy; 2024 Express Demo</p>
